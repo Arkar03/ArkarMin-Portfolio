79a69484b866e8c2f5dfc5ac4f27145389aa8bdf
Arkar <arkarmin.mi@gmail.com>
2024-01-21 18:25:27 +0630

    changes: updated cv

--- app/akmcv/page.jsx
+++ app/akmcv/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CV = () => {
   return (
     <div className="w-full h-[800px]">
-      <iframe src="pdf/ArkarMin'sCV.pdf" frameborder="0" className="w-full h-[800px]"></iframe>
+      <iframe src="pdf/Arkar'sCV.pdf" frameborder="0" className="w-full h-[800px]"></iframe>
     </div>
   );
 };
